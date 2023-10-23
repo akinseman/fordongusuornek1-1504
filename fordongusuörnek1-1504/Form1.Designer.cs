70f5a3e3822066a2d1aee7d4d0b3e5b8a523cf6f
akinseman <145451296+akinseman@users.noreply.github.com>
2023-10-23 12:31:14 +0300

    Add files via upload

---
+++ fordongusuörnek1-1504/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace fordongusuörnek1_1504
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnekle = new System.Windows.Forms.Button();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.lbad = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnekle
+            // 
+            this.btnekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnekle.Location = new System.Drawing.Point(12, 118);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(100, 76);
+            this.btnekle.TabIndex = 0;
+            this.btnekle.Text = "Yaz";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // txtad
+            // 
+            this.txtad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtad.Location = new System.Drawing.Point(12, 73);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 29);
+            this.txtad.TabIndex = 1;
+            this.txtad.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbad
+            // 
+            this.lbad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbad.FormattingEnabled = true;
+            this.lbad.ItemHeight = 24;
+            this.lbad.Location = new System.Drawing.Point(149, 64);
+            this.lbad.Name = "lbad";
+            this.lbad.Size = new System.Drawing.Size(120, 172);
+            this.lbad.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(329, 482);
+            this.Controls.Add(this.lbad);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.btnekle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.ListBox lbad;
+    }
+}
+
---
+++ fordongusuörnek1-1504/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fordongusuörnek1_1504
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            +
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            string ad = txtad.Text;
+
+            for (int i = 0; i<10; i++)
+            {
+                string metin = i.ToString() +"-" + ad;
+        }
+            { 
+    }
+}
